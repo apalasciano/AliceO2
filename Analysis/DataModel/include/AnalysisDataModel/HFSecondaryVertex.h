66ac3592e31678675dbac354982a410bc4ce306e
apalasci <antonio.palasciano@cern.ch>
2021-05-04 19:09:44 +0200

    Clang style

--- Analysis/DataModel/include/AnalysisDataModel/HFSecondaryVertex.h
+++ Analysis/DataModel/include/AnalysisDataModel/HFSecondaryVertex.h
@@ -313,14 +313,14 @@ DECLARE_SOA_TABLE(HfCandProng2MCRec, "AOD", "HFCANDP2MCREC",
 DECLARE_SOA_TABLE(HfCandProng2MCGen, "AOD", "HFCANDP2MCGEN",
                   hf_cand_prong2::FlagMCMatchGen,
                   hf_cand_prong2::OriginMCGen);
-                  
+
 // specific BPlus candidate properties
 namespace hf_cand_bplus
 {
 DECLARE_SOA_INDEX_COLUMN_FULL(Index0, index0, int, HfCandProng2, "_0"); // D0 index
 // MC matching result:
-DECLARE_SOA_COLUMN(FlagMCMatchRec, flagMCMatchRec, int8_t);         // reconstruction level
-DECLARE_SOA_COLUMN(FlagMCMatchGen, flagMCMatchGen, int8_t);         // generator level
+DECLARE_SOA_COLUMN(FlagMCMatchRec, flagMCMatchRec, int8_t); // reconstruction level
+DECLARE_SOA_COLUMN(FlagMCMatchGen, flagMCMatchGen, int8_t); // generator level
 
 enum DecayType { BPlusToD0Pi = 0 };
 } // namespace hf_cand_bplus
